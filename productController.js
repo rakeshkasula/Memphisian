da538eed18fc8dbc274d6ed620ec398331983ef0
rakeshkasula <rakeshkasula967@gmail.com>
2025-12-02 18:58:18 -0600

    Implement product controller with CRUD operations

---
+++ productController.js
@@ -0,0 +1,20 @@
+import Product from "../models/Product.js";
+
+export const getProducts = async (req, res) => {
+  const products = await Product.find();
+  res.json(products);
+};
+
+export const getProduct = async (req, res) => {
+  const product = await Product.findById(req.params.id);
+  res.json(product);
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.json(product);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
